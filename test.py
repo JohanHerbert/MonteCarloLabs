4e7520bcb381f97be0179cc09544757218a8d7ef
Johan Herbert <johanherbert99@gmail.com>
2024-05-21 13:11:57 +0200

    My initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#%matplotlib inline
+
+
+#from scipy.stats import norm, lognorm # gives us the normal and log normal pdf,cdf and inverse cdf (ppf,point percent function)
+#from scipy.stats.mstats import gmean #geomeetric mean
+print('hello')
+
